ae0d911b7908c970e7ae78121960afbcad1ed851
Brendan O'Connell <bmoconnell15@gmail.com>
2020-10-14 19:31:28 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,5 +1,8 @@
 # Your code here
 # For output purposes, use "puts" instead of "print" or "p"
 def meal_choice(vegetable, carb, protein="meat")
-  puts "A plate of #{protein} with #{vegetable} and #{carb}."
+  puts ""
+  statement = "A plate of #{protein} with #{vegetable} and #{carb}."
+  puts statement
+  statement
 end
